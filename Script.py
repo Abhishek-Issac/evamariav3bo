6a24973f30d2d8b2bf7f02d16caff805211ca8ac
200920082007 <93928612+200920082007@users.noreply.github.com>
2021-11-30 18:34:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,6 +1,6 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-MY NAME IS EVA MARIA,I CAN PROVIDE MOVIES,𝙹UST ADD ME TO YOUR GROUP"""
+MY NAME IS EVA MARIA,I CAN PROVIDE MOVIES,𝙹UST ADD ME TO YOUR GROUP AND MAKE ME ADMIN AND ENJOY"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
